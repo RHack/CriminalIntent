441a623d34af316bda4b775771442c4d3a1011df
RHack <robert.hackemack@gmail.com>
2017-08-08 21:22:02 -0700

    Created recycler view and added it to the CrimeListFragment.

--- app/src/main/java/com/example/envoy/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/envoy/criminalintent/CrimeListFragment.java
@@ -1,7 +1,13 @@
 package com.example.envoy.criminalintent;
 
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Envoy on 8/8/17.
@@ -9,6 +15,16 @@ import android.support.v4.app.Fragment;
 
 public class CrimeListFragment extends Fragment {
 
-    // This doesn't do anything yet.
+    private RecyclerView mCrimeRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
 
 }
